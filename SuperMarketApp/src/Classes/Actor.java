130f1f2313f0d878cf79fc84b61d1ad86a0999e4
alexeyb7 <alexeyb7@gmail.com>
2023-08-30 22:15:53 +0300

    Add SuperMarket App

---
+++ SuperMarketApp/src/Classes/Actor.java
@@ -0,0 +1,19 @@
+package Classes;
+
+import Interfaces.iActorBehaviour;
+
+public abstract class Actor implements iActorBehaviour {
+
+    protected String name;
+    protected boolean isTakeOrder;
+    protected boolean isMakeOrder;
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    abstract public void setName(String name);
+
+    abstract public String getName();
+
+}
