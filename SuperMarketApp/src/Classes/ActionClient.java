6a5fa3774f46f678c5bdb60bc4629a0112b3ecb2
alexeyb7 <alexeyb7@gmail.com>
2023-09-01 00:25:02 +0300

    Initial Adding ActionClient.java

---
+++ SuperMarketApp/src/Classes/ActionClient.java
@@ -0,0 +1,52 @@
+package Classes;
+
+public class ActionClient extends Actor {
+
+    public ActionClient(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isTakeOrder'");
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMakeOrder'");
+    }
+
+    @Override
+    public void setTakeOrder(boolean take) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setTakeOrder'");
+    }
+
+    @Override
+    public void setMakeOrder(boolean make) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setMakeOrder'");
+    }
+
+    @Override
+    public Actor getActor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getActor'");
+    }
+
+    @Override
+    public void setName(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setName'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+}
