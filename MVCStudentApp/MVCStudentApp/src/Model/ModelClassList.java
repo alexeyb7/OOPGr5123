ac9067cc9b9b245d811ddc08f003985f0d4e7120
alexeyb7 <alexeyb7@gmail.com>
2023-09-10 23:36:27 +0300

    Initial commit MVCStudentApp

---
+++ MVCStudentApp/MVCStudentApp/src/Model/ModelClassList.java
@@ -0,0 +1,18 @@
+package Model;
+
+import java.util.List;
+
+import Model.Core.Student;
+
+public class ModelClassList {
+
+    private List<Student> students;
+
+    public ModelClassList(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+}
