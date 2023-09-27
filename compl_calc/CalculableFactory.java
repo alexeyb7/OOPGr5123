d328e5479a2caad5047d908344cbf5f14fca6c60
alexeyb7 <alexeyb7@gmail.com>
2023-09-27 20:22:16 +0300

    Initial commit

---
+++ compl_calc/CalculableFactory.java
@@ -0,0 +1,6 @@
+    public class CalculableFactory implements ICalculableFactory {
+        public Calculable create(int primaryArg) {
+            return new Calculator(primaryArg);
+        }
+    }
+
